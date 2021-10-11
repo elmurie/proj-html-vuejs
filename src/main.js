e5be5b4ef2549a0d873eb9cdf35a8f3df0f6bc70
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-11 22:56:15 +0200

    imported fa icons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faClock, faLifeRing, faFileAlt, faUser, faCalendar, faEye } from '@fortawesome/free-regular-svg-icons'
+import { faFacebookF, faTwitter, faInstagram, faLinkedin, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
+import { faRulerCombined, faBook, faShoppingCart, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add( faClock, faTwitter, faFacebookF, faInstagram, faLinkedin, faLifeRing, faRulerCombined, faBook, faShoppingCart, faFileAlt, faUser, faCalendar, faEye, faMapMarkerAlt, faFacebookSquare )
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
