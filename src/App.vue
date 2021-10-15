1b79deaf26c726c2786228905fc3d976654e605c
elmurie <83522485+elmurie@users.noreply.github.com>
2021-10-15 18:19:58 +0200

    removed position fixed icons

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,6 @@
     <Header :links="headerLinks"/>
     <Main/>
     <Footer :links="footerLinks"/>
-    <!-- Fixed icons to the right -->
-    <ul class="icons-fixed d-flex flex-column align-items-center">
-        <li><a href="#"><font-awesome-icon :icon="['fas', 'ruler-combined']"/></a></li>
-        <li><a href="#"><font-awesome-icon :icon="['far', 'life-ring']"/></a></li>
-        <li><a href="#"><font-awesome-icon :icon="['fas', 'book']"/></a></li>
-        <li><a href="#"><font-awesome-icon :icon="['fas', 'shopping-cart']"/></a></li>
-    </ul>
   </div>
 </template>
 
@@ -55,21 +48,4 @@ export default {
 
 <style lang="scss">
 @import './assets/style/common.scss';
-  #app {
-    position : relative;
-    .icons-fixed {
-            position: fixed;
-            z-index: 10;
-            background-color: $mainBGC;
-            right: 0;
-            top: 12.2%;
-            padding : .3125rem;
-            box-shadow: -0.25rem .25rem .9375rem .1875rem #0000004f;
-            border-radius: .3125rem 0 0 .3125rem;
-
-            & li {
-                margin : .4em 0;
-            }
-        }
-  }
 </style>
